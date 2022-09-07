a2504506458068389d32a2973d4d58a42d2d7d5d
Dmitriy <dfeeler@yandex.ru>
2022-09-07 23:36:16 +0500

    add exmpls 12-15-16

--- Example010_/Program.cs
+++ Example010_/Program.cs
@@ -1,10 +1,10 @@
 ﻿int[] array = {11, 21, 31, 41, 15, 61, 23, 29, 55};
-
+int find = int.Parse(Console.ReadLine());
 int n = array.Length;
 int index = 0;
 while (index<n)
 {
-    if(array[index]==find)
+    if(array[index] == find)
     {
         Console.WriteLine(index);
         break;
